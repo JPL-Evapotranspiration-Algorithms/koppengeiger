3996967e458232638308d87527d3b52f73000f90
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2024-07-19 15:31:07 -0700

    including example

--- koppengeiger/koppengeiger.py
+++ koppengeiger/koppengeiger.py
@@ -5,7 +5,28 @@ from rasters import Raster, RasterGeometry
 
 KOPPEN_GEIEGER_FILENAME = join(abspath(dirname(__file__)), "KG_simplified_uint8.tif")
 
-def load_koppen_geiger(geometry: RasterGeometry = None):
+def load_koppen_geiger(geometry: RasterGeometry = None) -> Raster:
+    """
+    Load the Köppen-Geiger land-cover classification raster.
+
+    This function loads a subset of the Köppen-Geiger land-cover classification raster as a `rasters.Raster` object given a spatial area defined by `rasters.RasterGeometry` object.
+
+    The Köppen-Geiger land-cover classification is a widely used vegetation and climate classification system. More details can be found at https://koppen.earth/.
+
+    Parameters
+    ----------
+    geometry : RasterGeometry, optional
+        The geometry to which the raster should be transformed. If None, the original raster geometry is used.
+
+    Returns
+    -------
+    Raster
+        The loaded Köppen-Geiger land-cover classification raster, potentially transformed to the given geometry.
+
+    References
+    ----------
+    Beck, H. E., Zimmermann, N. E., McVicar, T. R., Vergopolan, N., Berg, A., & Wood, E. F. (2018). Present and future Köppen-Geiger climate classification maps at 1-km resolution. Scientific Data, 5, 180214.
+    """
     koppen_geiger = Raster.open(KOPPEN_GEIEGER_FILENAME)
 
     if geometry is not None:
